0b759126500ab5e35ab547582ccdc1bf2ba5b796
Suhail Khan <26044674+suhailskhan@users.noreply.github.com>
2022-07-14 00:16:10 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Suhail Khan on 7/14/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
