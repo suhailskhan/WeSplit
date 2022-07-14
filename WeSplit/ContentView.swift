0379794fc619c1c13dcd1899f770fdf03384615b
Suhail Khan <26044674+suhailskhan@users.noreply.github.com>
2022-07-14 01:09:58 -0700

    Clean up – Stage 1 (Day 16) complete

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,20 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry", "Hermione", "Ron"]
-        @State private var selectedStudent = "Harry"
-
-        var body: some View {
-            NavigationView {
-                Form {
-                    Picker("Select your student", selection: $selectedStudent) {
-                        ForEach(students, id: \.self) {
-                            Text($0)
-                        }
-                    }
-                }
-            }
-        }
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
